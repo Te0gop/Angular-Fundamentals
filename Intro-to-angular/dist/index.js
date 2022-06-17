abe558d0dc4601f878897755fb13b1d663319b67
unknown <Te0gop.u@gmail.com>
2022-06-17 13:51:20 +0300

    ts demo

---
+++ Intro-to-angular/dist/index.js
@@ -0,0 +1,10 @@
+var User = (function () {
+    function User(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+    return User;
+}());
+var ivan = new User("Ivan", 30);
+console.log(ivan.name);
+//# sourceMappingURL=index.js.map
---
+++ Intro-to-angular/src/index.ts
@@ -0,0 +1,15 @@
+import _ from 'underscore';
+
+class User {
+
+// name: string;
+// age: number;
+
+    constructor(public name: string, public age: number) {
+        // this.name = name; this.age = age;
+    } 
+}
+
+const ivan = new User("Ivan", 30);
+
+console.log(ivan.name);
