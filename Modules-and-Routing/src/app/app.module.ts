97c3d104ea5b52ea930ab515b4ee040e0d8cb669
unknown <Te0gop.u@gmail.com>
2022-06-21 13:30:22 +0300

    modifying components

--- Modules-and-Routing/src/app/app.module.ts
+++ Modules-and-Routing/src/app/app.module.ts
@@ -5,16 +5,22 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
+import { CoreModule } from './core/core.module';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    AboutComponent
+    AboutComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CoreModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- Modules-and-Routing/src/app/home/home.component.ts
+++ Modules-and-Routing/src/app/home/home.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { interval, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnDestroy {
+  counter = 0;
 
-  constructor() { }
+  intervalSubscribtion: Subscription;
 
-  ngOnInit(): void {
+  constructor() { 
+    this.intervalSubscribtion = interval(1000).subscribe(() => { this.counter++; });
+  }
+
+
+  ngOnDestroy(): void {
+    this.intervalSubscribtion.unsubscribe();
   }
 
 }
