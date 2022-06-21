d19e4c9ec43d84a13ffe7593fecd0d333662ffc9
unknown <Te0gop.u@gmail.com>
2022-06-21 13:37:26 +0300

    creating new components

--- Modules-and-Routing/src/app/app.module.ts
+++ Modules-and-Routing/src/app/app.module.ts
@@ -14,7 +14,7 @@ import { SharedModule } from './shared/shared.module';
     AppComponent,
     HomeComponent,
     AboutComponent,
-    NotFoundComponent
+    NotFoundComponent,
   ],
   imports: [
     BrowserModule,
